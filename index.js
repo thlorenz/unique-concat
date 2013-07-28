5c91f3b8319e431a5350d29ccdc20df9fcb5fcde
Thorsten Lorenz <thlorenz@gmx.de>
2013-07-28 10:02:52 -0400

    Array.isArray shim

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 'use strict';
 
+ var isArray = typeof Array.isArray === 'function'
+      ? Array.isArray
+      : function (a) { return typeof a === 'object' && !!a.length; };
+
 var go = module.exports = function (arr1, arr2) {
 
   if (!arr1 || !arr2) throw new Error('Need two arrays to merge');
-  if (!Array.isArray(arr1)) throw new Error('First argument is not an array, but a ' + typeof arr1);
-  if (!Array.isArray(arr2)) throw new Error('Second argument is not an array, but a ' + typeof arr2);
+  if (!isArray(arr1)) throw new Error('First argument is not an array, but a ' + typeof arr1);
+  if (!isArray(arr2)) throw new Error('Second argument is not an array, but a ' + typeof arr2);
 
   function hashify(acc, k) {
     acc[k] = true;
